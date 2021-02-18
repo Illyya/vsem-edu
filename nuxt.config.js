e82a3250d427e02ca7a1ee9c2d6839dac5a137ae
Илья Дубов <dubovilya90@gmail.com>
2021-02-18 10:45:14 +0300

    добавил папку docs

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,4 +37,14 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+
+  target: 'static',
+
+  router: {
+    base: '/vsem-edu/',
+  },
+
+  generate: {
+    dir: 'docs',
+  },
 }
